22b7b2278c30f2a84e9df2a8c891595e27fefe9d
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-01 06:16:37 +0900

    davinci動いた

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -2,6 +2,8 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
+  basePath: "https://api.openai.iniad.org/api/v1",
+  
 });
 const openai = new OpenAIApi(configuration);
 
