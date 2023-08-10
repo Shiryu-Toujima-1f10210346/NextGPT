752575f0b00bce624730471b790d41c9e4cb0509
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-11 01:31:15 +0900

    temprature:0を実現するために履歴を元に会話しなくした

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,31 +5,31 @@ const configuration = new Configuration({
   basePath: "https://api.openai.iniad.org/api/v1",
 });
 const openai = new OpenAIApi(configuration);
-const messagesHistory = [
+const systemText =
+  // messagesHistory => systemText
   {
     role: "system",
     content: `
 これはお題当てゲームです。
-ユーザーの入力したもの以外に､｢お題｣と｢NGワード｣があります。
+以下の形式で送られてきます｡
+"ユーザーの入力:" ここにユーザーの入力が入ります｡例:｢赤くて丸い､甘い果物｣
+"お題:" ここにお題が入ります｡例:｢リンゴ｣
+"NGワード:" ここにNGワードが入ります｡カンマ区切りで複数のNGワードが入ります｡例:｢赤い,くだもの｣
 ユーザーはお題を引き出そうとあなたに指示を出します。
 例えばお題が｢りんご｣だとすると､ユーザーは
 ｢赤くて丸い､甘い果物は？｣
 というような指示を出すでしょう。
-その指示に対してあなたはそれにあてはまる単語を返します。
-この場合はあなたは｢りんご｣とのみ返答してください｡
-できるだけお題ではない単語を返答してください｡
-ですが､一般的ではないものは返答しないでください｡
-NGワードに近い単語が含まれていた場合は､｢NGワードに類する単語が含まれています｡｣
+その指示に対してあなたはその特徴にあてはまる単語を返します。
+この例の場合はあなたは｢りんご｣とのみ返答してください｡
+ですが､あまり一般的ではないものは返答しないでください｡
+ユーザーの入力した文字列内にNGワードのどれか一つに近い単語が含まれていた場合は､｢NGワードに類する単語が含まれています｡｣
 と返してください｡
-例えばNGワードが｢りんご｣だとすると､
-｢リンゴ｣｢林檎｣｢Ringo｣｢Apple｣など｢りんご｣と同じ意味を示す単語はNGワードに類する単語です｡
-例えばNGワードが｢黄色｣だとすると､
-｢きいろ｣｢Yellow｣｢Kiiro｣と同じ意味を示す単語はNGワードに類する単語です｡
-NGワードのひらがなやカタカナや漢字､ローマ字や英語､言い換え表現をNGワードに類する単語として扱ってください｡
+例えばNGワードが｢りんご｣だとすると､｢リンゴ｣｢林檎｣｢Ringo｣｢Apple｣など｢りんご｣と同じ意味を示す単語はNGワードに類する単語です｡
+例えばNGワードが｢黄色｣だとすると､｢きいろ｣｢Yellow｣｢Kiiro｣と同じ意味を示す単語はNGワードに類する単語です｡
+NGワードのひらがなやカタカナや漢字､ローマ字や英語､言い換え表現もNGワードに類する単語として扱ってください｡
 日本語で返答してください。
 `,
-  },
-];
+  };
 
 const userMessageHistory = [];
 export default async function (req, res) {
@@ -55,23 +55,18 @@ export default async function (req, res) {
   }
 
   try {
-    messagesHistory.push({ role: "user", content: `${user}` });
     userMessageHistory.push(user);
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      // model: "gpt-4",
-      messages: messagesHistory,
+      messages: [systemText, { role: "user", content: `${user}` }],
+      temperature: 0,
     });
-    messagesHistory.push(completion.data.choices[0].message);
-    console.log(messagesHistory);
     console.log(completion.data.choices[0].message.content);
+    console.log([systemText, { role: "user", content: `${user}` }]);
     resText = completion.data.choices[0].message.content;
-    // console.log(completion.data);
     res.status(200).json({ result: completion.data.choices[0].message });
-    console.log(resText);
     return resText;
   } catch (error) {
-    // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
