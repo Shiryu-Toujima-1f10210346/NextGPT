05340285e8711aa736b6e989c828c12b3506360e
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-14 16:10:41 +0900

    遷移可能に

--- pages/components/Sidebar.js
+++ pages/components/Sidebar.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import SidebarData from './SidebarData'
+import {SidebarData} from './SidebarData'
+import Link from 'next/link';
 function Sidebar() {
   return (
     <div>
       <ul>
-        <li>ゲーム</li>
-        <li>ランキング</li>
+        {SidebarData.map((val, key) => {
+          return (
+            <div className='p-2 m-2'>
+            <li key = {key} onClick={() => {window.location.pathname = val.path}}>
+              <div>{val.icon}</div>
+              <div>{val.title}</div>
+            </li>
+            </div>
+          );
+        })}
       </ul>
     </div>
   )
