1bc94fa9377882016c98be0aec78e8a8eb438244
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-14 18:42:34 +0900

    よくわからん

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Sideber from '../components/Sidebar'
+import Sideber from "../components/Sidebar"
 import Head from 'next/head'
+import global from '../styles/global.module.css'
 
 function index() {
   return (
@@ -11,6 +12,17 @@ function index() {
         <script src="https://cdn.tailwindcss.com"></script>
         </Head>
         <Sideber />
+        <main>
+            <div className={global.container}>
+                <p>このサイトはGPTとお題当てゲームができるサイトです</p>
+                <p>･ルール NGワードに近い言葉を使わずにGPTからお題の単語を引き出そう！</p>
+                <p>例) お題: お寿司</p>
+                <p>NGワード: 寿司 魚 回転 シャリ ネタ</p>
+                <p>指示文: 
+
+                </p>
+            </div>
+        </main>
     </div>
   )
 }
