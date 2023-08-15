dd4fcba299be87ab89dfe032e86ae8ea20a8c4bd
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-16 05:20:38 +0900

    デバッグページ

---
+++ pages/debug/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import React from 'react'
+import Sidebar from '../../components/Sidebar'
+import global from '../../styles/global.module.css'
+
+function debug() {
+  return (
+    <div>
+        <Head>
+        <title>デバッグ</title>
+        <link rel="icon" href="/gpt.png" />
+        <script src="https://cdn.tailwindcss.com"></script>
+        </Head>
+        <Sidebar />
+        <main>
+            <div className={global.container}>
+                
+            </div>
+        </main>
+    </div>
+  )
+}
+
+export default debug
