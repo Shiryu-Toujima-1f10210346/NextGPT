07a65386785bd491be335237eb145ac3f102f378
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-08-17 20:49:35 +0900

    fix conflict

--- pages/debug/index.tsx
+++ pages/debug/index.tsx
@@ -1,29 +1,29 @@
-import Head from 'next/head'
-import React from 'react'
-import Sidebar from '../../components/Sidebar'
-import global from '../../styles/global.module.css'
+import Head from "next/head";
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+import global from "../../styles/global.module.css";
 
+let score = 0;
 function debug() {
   return (
     <div>
-        <Head>
+      <Head>
         <title>デバッグ</title>
         <link rel="icon" href="/gpt.png" />
         <script src="https://cdn.tailwindcss.com"></script>
-        </Head>
-        <Sidebar />
-        <main>
-            <div className={global.container}>
-                デバッグ用ページやで
-            </div>
-            <input onChange={(e) => score = Number(e.target.value)} />
-            <div>{score}</div>
-            <button onClick={() => addRanking('test', score)}>ランキング更新</button>
-                <p>デバッグ用ページ</p>
-            </div>
-        </main>
+      </Head>
+      <Sidebar />
+      <main>
+        <div className={global.container}>デバッグ用ページやで</div>
+        <input onChange={(e) => (score = Number(e.target.value))} />
+        <div>{score}</div>
+        <button onClick={() => addRanking("test", score)}>
+          ランキング更新
+        </button>
+        <p>デバッグ用ページ</p>
+      </main>
     </div>
-  )
+  );
 }
 
-export default debug
+export default debug;
