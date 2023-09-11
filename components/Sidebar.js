015bc642fa3d1a4cc5f5c751694b74c92acb750b
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-09-11 14:14:10 +0900

    サイドバー改修

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,15 +2,16 @@ import React from "react";
 import { SidebarData } from "./SidebarData";
 import styles from "./Sidebar.module.css";
 import Head from "next/head";
-
+import { useRouter } from "next/router";
+import Link from "next/link";
 // css
 
 function Sidebar() {
+  const router = useRouter();
   return (
     <div>
       <Head>
         <script src="https://cdn.tailwindcss.com"></script>
-        
       </Head>
       <ul className={styles.sidebar}>
         {SidebarData.map((val, key) => {
@@ -18,16 +19,24 @@ function Sidebar() {
             <div className="">
               <li
                 key={key}
-                onClick={() => {
-                  window.location.pathname = val.path;
-                }}
-                className="
-             md:m-4 lg:m-4 object-center flex flex-row justify-center items-center
-              hover:bg-gray-200 rounded-xl text-gray-800
-              lg:p-6 shadow-md hover:shadow-2xl py-4
-              "
+                className={`md:m-4 lg:m-4 
+                object-center 
+                flex flex-row justify-center items-center 
+                
+                text-gray-800 
+                lg:p-6 shadow-md 
+                hover:shadow-2xl py-4 rounded-xl  ${
+                  router.pathname == val.path
+                    ? "bg-gray-900"
+                    : "hover:bg-gray-200 "
+                }`}
               >
-                <div className={styles.icon}>{val.icon}</div>
+                <Link href={val.path}>
+                  <div className={styles.icon}>
+                    {/* urlとval.idが一緒なら */}
+                    {router.pathname == val.path ? val.seleced : val.icon}
+                  </div>
+                </Link>
                 <div className={styles.title}>{val.title}</div>
               </li>
             </div>
--- components/SidebarData.js
+++ components/SidebarData.js
@@ -8,33 +8,45 @@ import AddCardIcon from "@mui/icons-material/AddCard";
 
 export const SidebarData = [
   {
-    title: "Home",
+    id: "home",
+    title: "ホーム",
     icon: <HomeSharpIcon />,
+    seleced: <HomeSharpIcon style={{ color: "white" }} />,
     path: "/",
   },
   {
+    id: "game",
     title: "ゲーム",
     icon: <SportsEsportsSharpIcon />,
+    seleced: <SportsEsportsSharpIcon style={{ color: "white" }} />,
     path: "/game",
   },
   {
+    id: "rank",
     title: "ランキング",
     icon: <EmojiEventsSharpIcon />,
+    seleced: <EmojiEventsSharpIcon style={{ color: "white" }} />,
     path: "/rank",
   },
   {
-    title: "About",
+    id: "info",
+    title: "インフォ",
     icon: <ThreePSharpIcon />,
+    seleced: <ThreePSharpIcon style={{ color: "white" }} />,
     path: "/about",
   },
   {
+    id: "debug",
     title: "デバッグ",
     icon: <AdbIcon />,
+    seleced: <AdbIcon style={{ color: "white" }} />,
     path: "/debug",
   },
   {
+    id: "odai",
     title: "お題",
     icon: <AddCardIcon />,
+    seleced: <AddCardIcon style={{ color: "white" }} />,
     path: "/odai",
   },
 ];
