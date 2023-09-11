419b2e2f756729c27d33cd3bd99b3d49a383d89f
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-09-11 15:18:04 +0900

    typo fix

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -22,22 +22,26 @@ function Sidebar() {
                 className={`md:m-4 lg:m-4 
                 object-center 
                 flex flex-row justify-center items-center 
-                
+                lg:justify-around lg:text-xl
                 text-gray-800 
                 lg:p-6 shadow-md 
-                hover:shadow-2xl py-4 rounded-xl  ${
+                hover:shadow-2xl py-4 rounded-full ${
                   router.pathname == val.path
-                    ? "bg-gray-900"
+                    ? "bg-gray-900 text-white"
                     : "hover:bg-gray-200 "
                 }`}
               >
                 <Link href={val.path}>
                   <div className={styles.icon}>
                     {/* urlとval.idが一緒なら */}
-                    {router.pathname == val.path ? val.seleced : val.icon}
+                    {router.pathname == val.path ? val.selected : val.icon}
                   </div>
                 </Link>
-                <div className={styles.title}>{val.title}</div>
+                {router.pathname == val.path ? (
+                  ""
+                ) : (
+                  <div className={`${styles.title} ml-4`}>{val.title}</div>
+                )}
               </li>
             </div>
           );
