3368f3f9140f3569fa633a8cf2329aab617c96aa
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-09-29 14:20:04 +0900

    checkedのみ表示するように

--- pages/github/index.tsx
+++ pages/github/index.tsx
@@ -9,20 +9,28 @@ const HomePage = () => {
 
   useEffect(() => {
     // GitHubのAPIエンドポイントURL
-    const apiUrl =
+    const apiIssueUrl =
       "https://api.github.com/repos/Shiryu-Toujima-1f10210346/NextGPT/issues";
 
     // GitHubアクセストークン
     const accessToken = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN;
 
     // APIリクエストを送信
-    fetch(apiUrl, {
+    fetch(apiIssueUrl, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     })
       .then((response) => response.json())
-      .then((data) => setIssues(data))
+      .then((data) => {
+        const filteredIssues = data.filter(
+          (issue: { labels: { name: string }[] }) =>
+            issue.labels.some(
+              (label: { name: string }) => label.name === "checked"
+            )
+        );
+        setIssues(filteredIssues);
+      })
       .catch((error) => console.error("Error fetching data:", error));
     console.table(issues);
   }, []);
@@ -34,22 +42,22 @@ const HomePage = () => {
         <ul>
           <li>
             <a
-              href="https://github.com/Shiryu-Toujima-1f10210346/NextGPT/issues/new?assignees=&labels=feature-request&projects=&template=feature_request.yml"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-green-500 underline"
-            >
-              機能追加のリクエストはこちらから
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/Shiryu-Toujima-1f10210346/NextGPT/issues/new?assignees=&labels=bug&projects=&template=bug_report.yml"
+              href="https://github.com/Shiryu-Toujima-1f10210346/NextGPT/issues/new?assignees=&labels=bug&projects=&template=bug-report.md&title="
               target="_blank"
               rel="noopener noreferrer"
               className="text-red-500 underline"
             >
               バグ報告はこちらから
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/Shiryu-Toujima-1f10210346/NextGPT/issues/new?assignees=&labels=enhancement&projects=&template=fature-request.md&title="
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-500 underline"
+            >
+              機能追加のリクエストはこちらから
             </a>
           </li>
           <li>
@@ -66,7 +74,11 @@ const HomePage = () => {
         <h1>既知のバグ</h1>
         <ul>
           {issues.map((issue) => {
-            if (issue.labels.some((label) => label.name === "bug")) {
+            if (
+              issue.labels.some(
+                (label: { name: string }) => label.name === "bug"
+              )
+            ) {
               return (
                 <li key={issue.id}>
                   <span>･</span>
@@ -75,7 +87,7 @@ const HomePage = () => {
                     href={issue.html_url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-red-500"
+                    className="text-red-500 underline"
                   >
                     {issue.title}
                   </a>
@@ -86,10 +98,14 @@ const HomePage = () => {
           })}
         </ul>
         <br />
-        <h1>機能要望一覧</h1>
+        <h1>機能要望</h1>
         <ul>
           {issues.map((issue) => {
-            if (issue.labels.some((label) => label.name === "enhancement")) {
+            if (
+              issue.labels.some(
+                (label: { name: string }) => label.name === "enhancement"
+              )
+            ) {
               return (
                 <li key={issue.id}>
                   <span>･</span>
@@ -98,7 +114,7 @@ const HomePage = () => {
                     href={issue.html_url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-green-500"
+                    className="text-green-500 underline"
                   >
                     {issue.title}
                   </a>
@@ -114,7 +130,11 @@ const HomePage = () => {
         <h1>改善案</h1>
         <ul>
           {issues.map((issue) => {
-            if (issue.labels.some((label) => label.name === "improve")) {
+            if (
+              issue.labels.some(
+                (label: { name: string }) => label.name === "improve"
+              )
+            ) {
               return (
                 <li key={issue.id}>
                   <span>･</span>
@@ -123,7 +143,7 @@ const HomePage = () => {
                     href={issue.html_url}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-blue-500"
+                    className="text-blue-500 underline"
                   >
                     {issue.title}
                   </a>
