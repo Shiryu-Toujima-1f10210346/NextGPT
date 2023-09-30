0be57a9118e2b7a04ac578b8c8a9d5d1035c6049
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-10-01 08:27:59 +0900

    サイドバー調整

--- components/SidebarData.js
+++ components/SidebarData.js
@@ -1,46 +1,41 @@
 import React from "react";
-import SportsEsportsSharpIcon from "@mui/icons-material/SportsEsportsSharp";
-import EmojiEventsSharpIcon from "@mui/icons-material/EmojiEventsSharp";
-import HomeSharpIcon from "@mui/icons-material/HomeSharp";
-import ThreePSharpIcon from "@mui/icons-material/ThreePSharp";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import HomeIcon from "@mui/icons-material/Home";
+import ThreePIcon from "@mui/icons-material/ThreeP";
 import AdbIcon from "@mui/icons-material/Adb";
 import AddCardIcon from "@mui/icons-material/AddCard";
+import InfoIcon from "@mui/icons-material/Info";
+import HistoryIcon from "@mui/icons-material/History";
 
 export const SidebarData = [
   // {
   //   id: "home",
   //   title: "ホーム",
-  //   icon: <HomeSharpIcon />,
-  //   selected: <HomeSharpIcon style={{ color: "white" }} />,
+  //   icon: <HomeIcon />,
+  //   selected: <HomeIcon style={{ color: "white" }} />,
   //   path: "/",
   // },
   {
     id: "resultList",
     title: "対戦履歴",
-    icon: <HomeSharpIcon style={{ color: "white" }} />,
-    selected: <HomeSharpIcon />,
+    icon: <HistoryIcon style={{ color: "white" }} />,
+    selected: <HistoryIcon />,
     path: "/resultList",
   },
   {
     id: "rank",
     title: "ランキング",
-    icon: <EmojiEventsSharpIcon style={{ color: "white" }} />,
-    selected: <EmojiEventsSharpIcon />,
+    icon: <EmojiEventsIcon style={{ color: "white" }} />,
+    selected: <EmojiEventsIcon />,
     path: "/rank",
   },
   {
     id: "game",
     title: "ゲーム",
-    icon: <SportsEsportsSharpIcon style={{ color: "white" }} />,
-    selected: <SportsEsportsSharpIcon />,
+    icon: <SportsEsportsIcon style={{ color: "white" }} />,
+    selected: <SportsEsportsIcon />,
     path: "/game",
-  },
-  {
-    id: "info",
-    title: "インフォ",
-    icon: <AdbIcon style={{ color: "white" }} />,
-    selected: <AdbIcon />,
-    path: "/info",
   },
   {
     id: "odai",
@@ -48,5 +43,12 @@ export const SidebarData = [
     icon: <AddCardIcon style={{ color: "white" }} />,
     selected: <AddCardIcon />,
     path: "/odai",
+  },
+  {
+    id: "info",
+    title: "インフォ",
+    icon: <InfoIcon style={{ color: "white" }} />,
+    selected: <InfoIcon />,
+    path: "/info",
   },
 ];
