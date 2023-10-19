71b8827b7baf656711bf39e8f37230cec8e68f2d
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-10-19 16:14:15 +0900

    Google Analtics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,48 @@
-import App from "next/app";
-App.getInitialProps = async () => ({ pageProps: {} });
-export default App;
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import App, { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  return { ...appProps };
+};
+
+export default MyApp;
